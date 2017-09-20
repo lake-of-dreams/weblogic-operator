70f2f6280a352fd13b9911852d8621b948dab4ac
Prashant_Bajpai <Prashant.Bajpai@oracle.com>
2017-09-20 13:36:09 +0530

    adding domain constants

--- pkg/constants/constants.go
+++ pkg/constants/constants.go
@@ -2,3 +2,5 @@ package constants
 
 // WeblogicServerLabel is applied to all components of a Weblogic Server
 const WeblogicServerLabel = "WeblogicServer.v1.weblogic.oracle.com"
+// WeblogicDomainLabel is applied to all components of a Weblogic Domain
+const WeblogicDomainLabel = "WeblogicDomain.v1.weblogic.oracle.com"
