f9000fc1b5c87cb2b162b3ec1fea2b6f83a28b7f
Prashant_Bajpai <Prashant.Bajpai@oracle.com>
2017-09-21 22:02:47 +0530

    adding necessary changes

--- pkg/constants/constants.go
+++ pkg/constants/constants.go
@@ -8,4 +8,10 @@ const (
 	WeblogicServerGroupName          = "weblogic.oracle.com"
 	WeblogicServerSchemeVersion      = "v1"
 	WeblogicImageName                = "docker.io/store/oracle/weblogic"
+
+	WebLogicDomainLabel              = "WebLogicDomain.v1.weblogic.oracle.com"
+	WebLogicDomainResourceKind       = "WebLogicDomain"
+	WebLogicDomainResourceKindPlural = "weblogicdomains"
+	WebLogicGroupName		         = "weblogic.oracle.com"
+	WeblogicDomainSchemeVersion      = "v1"
 )
