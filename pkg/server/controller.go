4fd421213d3a88d8b54e8806ee4e7d3e61427738
priyanka_p_prabhu <priyanka.p.prabhu@oracle.com>
2017-09-25 16:39:48 +0530

    added codelines for replica set and horizontalPodAutoscaling objects for weblogic-operator deployment.

--- pkg/server/controller.go
+++ pkg/server/controller.go
@@ -3,8 +3,7 @@ package server
 import (
 	"time"
 
-	"github.com/golang/glog"
-	"k8s.io/api/apps/v1beta1"
+	"k8s.io/api/extensions/v1beta1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/fields"
 	"k8s.io/apimachinery/pkg/runtime"
@@ -12,28 +11,42 @@ import (
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/cache"
-	"weblogic-operator/pkg/constants"
+	"github.com/golang/glog"
 	"weblogic-operator/pkg/types"
+	"weblogic-operator/pkg/constants"
+	"k8s.io/api/autoscaling/v1"
 )
 
 type StoreToWeblogicServerLister struct {
 	cache.Store
 }
 
-type StoreToWeblogicStatefulSetLister struct {
+//type StoreToWeblogicStatefulSetLister struct {
+//	cache.Store
+//}
+
+type StoreToWeblogicReplicaSetLister struct {
+	cache.Store
+}
+
+type StoreToWeblogicHorizontalPodAutoscalingLister struct {
 	cache.Store
 }
 
 // The WeblogicController watches the Kubernetes API for changes to Weblogic resources
 type WeblogicController struct {
-	client                        kubernetes.Interface
-	restClient                    *rest.RESTClient
-	startTime                     time.Time
-	shutdown                      bool
-	weblogicServerController      cache.Controller
-	weblogicServerStore           StoreToWeblogicServerLister
-	weblogicStatefulSetController cache.Controller
-	weblogicStatefulSetStore      StoreToWeblogicStatefulSetLister
+	client                        			kubernetes.Interface
+	restClient                    			*rest.RESTClient
+	startTime                     			time.Time
+	shutdown                      			bool
+	weblogicServerController      			cache.Controller
+	weblogicServerStore           			StoreToWeblogicServerLister
+	//weblogicStatefulSetController 			cache.Controller
+	//weblogicStatefulSetStore      			StoreToWeblogicStatefulSetLister
+	weblogicReplicaSet 						cache.Controller
+	weblogicReplicaSetStore					StoreToWeblogicReplicaSetLister
+	weblogicHorizontalPodAutoscaling 		cache.Controller
+	weblogicHorizontalPodAutoscalingStore	StoreToWeblogicHorizontalPodAutoscalingLister
 }
 
 // NewController creates a new WeblogicController.
@@ -57,26 +70,68 @@ func NewController(kubeClient kubernetes.Interface, restClient *rest.RESTClient,
 		resyncPeriod,
 		weblogicServerHandlers)
 
-	statefulSetHandler := cache.ResourceEventHandlerFuncs{
-		AddFunc:    m.onStatefulSetAdd,
-		DeleteFunc: m.onStatefulSetDelete,
-		UpdateFunc: m.onStatefulSetUpdate,
+	//statefulSetHandler := cache.ResourceEventHandlerFuncs{
+	//	AddFunc:    m.onStatefulSetAdd,
+	//	DeleteFunc: m.onStatefulSetDelete,
+	//	UpdateFunc: m.onStatefulSetUpdate,
+	//}
+	//
+	//m.weblogicStatefulSetStore.Store, m.weblogicStatefulSetController = cache.NewInformer(
+	//	&cache.ListWatch{
+	//		ListFunc: func(options metav1.ListOptions) (runtime.Object, error) {
+	//			options.LabelSelector = constants.WeblogicServerLabel
+	//			return kubeClient.AppsV1beta1().StatefulSets(namespace).List(options)
+	//		},
+	//		WatchFunc: func(options metav1.ListOptions) (watch.Interface, error) {
+	//			options.LabelSelector = constants.WeblogicServerLabel
+	//			return kubeClient.AppsV1beta1().StatefulSets(namespace).Watch(options)
+	//		},
+	//	},
+	//	&v1beta1.StatefulSet{},
+	//	resyncPeriod,
+	//	statefulSetHandler)
+
+	replicaSetHandler := cache.ResourceEventHandlerFuncs{
+		AddFunc:    m.onReplicaSetAdd,
+		DeleteFunc: m.onReplicaSetDelete,
+		UpdateFunc: m.onReplicaSetUpdate,
 	}
 
-	m.weblogicStatefulSetStore.Store, m.weblogicStatefulSetController = cache.NewInformer(
+	m.weblogicReplicaSetStore.Store, m.weblogicReplicaSet = cache.NewInformer(
 		&cache.ListWatch{
 			ListFunc: func(options metav1.ListOptions) (runtime.Object, error) {
 				options.LabelSelector = constants.WeblogicServerLabel
-				return kubeClient.AppsV1beta1().StatefulSets(namespace).List(options)
+				return kubeClient.ExtensionsV1beta1().ReplicaSets(namespace).List(options)
 			},
 			WatchFunc: func(options metav1.ListOptions) (watch.Interface, error) {
 				options.LabelSelector = constants.WeblogicServerLabel
-				return kubeClient.AppsV1beta1().StatefulSets(namespace).Watch(options)
+				return kubeClient.ExtensionsV1beta1().ReplicaSets(namespace).Watch(options)
 			},
 		},
-		&v1beta1.StatefulSet{},
+		&v1beta1.ReplicaSet{},
 		resyncPeriod,
-		statefulSetHandler)
+		replicaSetHandler)
+
+	horizontalPodAutoscalerHandler := cache.ResourceEventHandlerFuncs{
+		AddFunc:    m.onHorizontalPodAutoscalerAdd,
+		DeleteFunc: m.onHorizontalPodAutoscalerDelete,
+		UpdateFunc: m.onHorizontalPodAutoscalerUpdate,
+	}
+
+	m.weblogicHorizontalPodAutoscalingStore.Store, m.weblogicHorizontalPodAutoscaling = cache.NewInformer(
+		&cache.ListWatch{
+			ListFunc: func(options metav1.ListOptions) (runtime.Object, error) {
+				options.LabelSelector = constants.WeblogicServerLabel
+				return kubeClient.AutoscalingV1().HorizontalPodAutoscalers(namespace).List(options)
+			},
+			WatchFunc: func(options metav1.ListOptions) (watch.Interface, error) {
+				options.LabelSelector = constants.WeblogicServerLabel
+				return kubeClient.AutoscalingV1().HorizontalPodAutoscalers(namespace).Watch(options)
+			},
+		},
+		&v1.HorizontalPodAutoscaler{},
+		resyncPeriod,
+		horizontalPodAutoscalerHandler)
 
 	return &m, nil
 }
@@ -121,40 +176,100 @@ func (m *WeblogicController) onUpdate(old, cur interface{}) {
 	}
 }
 
-func (m *WeblogicController) onStatefulSetAdd(obj interface{}) {
-	glog.V(4).Info("WeblogicController.onStatefulSetAdd() called")
+//func (m *WeblogicController) onStatefulSetAdd(obj interface{}) {
+//	glog.V(4).Info("WeblogicController.onStatefulSetAdd() called")
+//
+//	statefulSet := obj.(*v1beta1.StatefulSet)
+//
+//	weblogicServer, err := GetServerForStatefulSet(statefulSet, m.restClient)
+//	if err != nil {
+//		// FIXME: Should we delete the stateful set here???
+//		// it has no server but it has the label.
+//		glog.Errorf("Failed to find server for stateful set: %s(%s):%#v", statefulSet.Name, err.Error(), statefulSet.Labels)
+//		return
+//	}
+//	err = updateServerWithStatefulSet(weblogicServer, statefulSet, m.client, m.restClient)
+//	if err != nil {
+//		glog.Errorf("Failed to create update Server: %s", err)
+//	}
+//}
+//
+////TODO Fix hanldings here. Need to call onStatefulSetAdd ???
+//func (m *WeblogicController) onStatefulSetDelete(obj interface{}) {
+//	glog.V(4).Info("WeblogicController.onStatefulSetDelete() called")
+//	m.onStatefulSetAdd(obj)
+//}
+//
+//func (m *WeblogicController) onStatefulSetUpdate(old, new interface{}) {
+//	glog.V(4).Info("WeblogicController.onStatefulSetUpdate() called")
+//	m.onStatefulSetAdd(new)
+//}
 
-	statefulSet := obj.(*v1beta1.StatefulSet)
+func (m *WeblogicController) onReplicaSetAdd(obj interface{}) {
+	glog.V(4).Info("WeblogicController.onReplicaSetAdd() called")
 
-	weblogicServer, err := GetServerForStatefulSet(statefulSet, m.restClient)
+	replicaSet := obj.(*v1beta1.ReplicaSet)
+
+	weblogicServer, err := GetServerForReplicaSet(replicaSet, m.restClient)
 	if err != nil {
-		// FIXME: Should we delete the stateful set here???
+		// FIXME: Should we delete the replica set here???
 		// it has no server but it has the label.
-		glog.Errorf("Failed to find server for stateful set: %s(%s):%#v", statefulSet.Name, err.Error(), statefulSet.Labels)
+		glog.Errorf("Failed to find server for replica set: %s(%s):%#v", replicaSet.Name, err.Error(), replicaSet.Labels)
 		return
 	}
-	err = updateServerWithStatefulSet(weblogicServer, statefulSet, m.client, m.restClient)
+	err = updateServerWithReplicaSet(weblogicServer, replicaSet, m.client, m.restClient)
 	if err != nil {
 		glog.Errorf("Failed to create update Server: %s", err)
 	}
 }
 
 //TODO Fix hanldings here. Need to call onStatefulSetAdd ???
-func (m *WeblogicController) onStatefulSetDelete(obj interface{}) {
-	glog.V(4).Info("WeblogicController.onStatefulSetDelete() called")
-	m.onStatefulSetAdd(obj)
+func (m *WeblogicController) onReplicaSetDelete(obj interface{}) {
+	glog.V(4).Info("WeblogicController.onReplicaSetDelete() called")
+	m.onReplicaSetAdd(obj)
 }
 
-func (m *WeblogicController) onStatefulSetUpdate(old, new interface{}) {
-	glog.V(4).Info("WeblogicController.onStatefulSetUpdate() called")
-	m.onStatefulSetAdd(new)
+func (m *WeblogicController) onReplicaSetUpdate(old, new interface{}) {
+	glog.V(4).Info("WeblogicController.onReplicaSetUpdate() called")
+	m.onReplicaSetAdd(new)
+}
+
+func (m *WeblogicController) onHorizontalPodAutoscalerAdd(obj interface{}) {
+	glog.V(4).Info("WeblogicController.onHorizontalPodAutoscalerAdd() called")
+
+	horizontalPodAutoscaler := obj.(*v1.HorizontalPodAutoscaler)
+
+	weblogicServer, err := GetServerForHorizontalPodAutoscaler(horizontalPodAutoscaler, m.restClient)
+	if err != nil {
+		// FIXME: Should we delete the replica set here???
+		// it has no server but it has the label.
+		glog.Errorf("Failed to find server for horizontal pod autoscaler: %s(%s):%#v", horizontalPodAutoscaler.Name, err.Error(), horizontalPodAutoscaler.Labels)
+		return
+	}
+	err = updateServerWithHorizontalPodAutoscaler(weblogicServer, horizontalPodAutoscaler, m.client, m.restClient)
+	if err != nil {
+		glog.Errorf("Failed to create update Server: %s", err)
+	}
+}
+
+//TODO Fix hanldings here. Need to call onStatefulSetAdd ???
+func (m *WeblogicController) onHorizontalPodAutoscalerDelete(obj interface{}) {
+	glog.V(4).Info("WeblogicController.onHorizontalPodAutoscalerDelete() called")
+	m.onHorizontalPodAutoscalerAdd(obj)
+}
+
+func (m *WeblogicController) onHorizontalPodAutoscalerUpdate(old, new interface{}) {
+	glog.V(4).Info("WeblogicController.onHorizontalPodAutoscalerUpdate() called")
+	m.onHorizontalPodAutoscalerAdd(new)
 }
 
 // Run the Weblogic controller
 func (m *WeblogicController) Run(stopChan <-chan struct{}) {
 	glog.Infof("Starting Weblogic controller")
 	go m.weblogicServerController.Run(stopChan)
-	go m.weblogicStatefulSetController.Run(stopChan)
+	//go m.weblogicStatefulSetController.Run(stopChan)
+	go m.weblogicReplicaSet.Run(stopChan)
+	go m.weblogicHorizontalPodAutoscaling.Run(stopChan)
 	<-stopChan
 	glog.Infof("Shutting down Weblogic controller")
 }
